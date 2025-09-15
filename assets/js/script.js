29577af83aa691da100bf3618feb49eda9a03fa1
salih <uixova.studio@gmail.com>
2025-09-15 20:35:50 +0300

    footer upgrade

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,28 @@
 function showSection(sectionId) {
-        const sections = document.querySelectorAll('.section');
-        sections.forEach(section => {
-            section.style.display = 'none';
-        });
-        document.getElementById(sectionId).style.display = 'block';
-    }
+  const sections = document.querySelectorAll('.section');
+  sections.forEach(section => {
+    section.style.display = 'none';
+  });
+  document.getElementById(sectionId).style.display = 'block';
+}
 
-    function goHome() {
-        showSection('home');
-    }
-    // Başlangıçta sadece anasayfa görünür
-    showSection('home');
-    // Galeri Lightbox
-    function openLightbox(img) {
+function goHome() {
+  showSection('home');
+}
+// Başlangıçta sadece anasayfa görünür
+showSection('home');
+// Galeri Lightbox
+function openLightbox(img) {
   document.getElementById("lightbox").style.display = "block";
   document.getElementById("lightbox-img").src = img.src;
-    }
-    function closeLightbox() {
+}
+function closeLightbox() {
   document.getElementById("lightbox").style.display = "none";
-    }
+}
+
+// Sayfa Bağlama
+fetch('page/galery.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('galery-section').innerHTML = data;
+  });
